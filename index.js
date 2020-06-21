9dfda5016c733516f2f5f67d92a91b4668f43cd2
umairJanjua <56170485+UmairJanjua6@users.noreply.github.com>
2020-06-21 16:12:08 +0500

    added JS code

---
+++ index.js
@@ -0,0 +1,131 @@
+function showText1() {
+    var element = document.getElementById("text1");
+
+    if(element.style.visibility == 'hidden') {
+        element.style.visibility = 'visible';
+    }
+    else {
+        element.style.visibility = 'hidden';
+    }
+}
+
+function showText2() {
+    var element = document.getElementById("text2");
+
+    if(element.style.visibility == 'hidden') {
+        element.style.visibility = 'visible';
+    }
+    else {
+        element.style.visibility = 'hidden';
+    }
+}
+
+function showText3() {
+    var element = document.getElementById("text3");
+
+    if(element.style.visibility == 'hidden') {
+        element.style.visibility = 'visible';
+    }
+    else {
+        element.style.visibility = 'hidden';
+    }
+}
+
+function showText4() {
+    var element = document.getElementById("text4");
+
+    if(element.style.visibility == 'hidden') {
+        element.style.visibility = 'visible';
+    }
+    else {
+        element.style.visibility = 'hidden';
+    }
+}
+
+function showText5() {
+    var element = document.getElementById("text5");
+
+    if(element.style.visibility == 'hidden') {
+        element.style.visibility = 'visible';
+    }
+    else {
+        element.style.visibility = 'hidden';
+    }
+}
+
+function showText6() {
+    var element = document.getElementById("text6");
+
+    if(element.style.visibility == 'hidden') {
+        element.style.visibility = 'visible';
+    }
+    else {
+        element.style.visibility = 'hidden';
+    }
+}
+
+function showText7() {
+    var element = document.getElementById("text7");
+
+    if(element.style.visibility == 'hidden') {
+        element.style.visibility = 'visible';
+    }
+    else {
+        element.style.visibility = 'hidden';
+    }
+}
+
+function showText8() {
+    var element = document.getElementById("text8");
+
+    if(element.style.visibility == 'hidden') {
+        element.style.visibility = 'visible';
+    }
+    else {
+        element.style.visibility = 'hidden';
+    }
+}
+
+function showText9() {
+    var element = document.getElementById("text9");
+
+    if(element.style.visibility == 'hidden') {
+        element.style.visibility = 'visible';
+    }
+    else {
+        element.style.visibility = 'hidden';
+    }
+}
+
+function showText10() {
+    var element = document.getElementById("text10");
+
+    if(element.style.visibility == 'hidden') {
+        element.style.visibility = 'visible';
+    }
+    else {
+        element.style.visibility = 'hidden';
+    }
+}
+
+function showText11() {
+    var element = document.getElementById("text11");
+
+    if(element.style.visibility == 'hidden') {
+        element.style.visibility = 'visible';
+    }
+    else {
+        element.style.visibility = 'hidden';
+    }
+}
+
+function showText12() {
+    var element = document.getElementById("text12");
+
+    if(element.style.visibility == 'hidden') {
+        element.style.visibility = 'visible';
+    }
+    else {
+        element.style.visibility = 'hidden';
+    }
+}
